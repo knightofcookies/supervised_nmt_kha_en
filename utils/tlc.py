d04fbe8665573b2073d42e4d32297b2ba71b896d
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-11-24 15:20:09 +0530

    Add unclean translated data

--- utils/tlc.py
+++ utils/tlc.py
@@ -9,6 +9,9 @@ for file in os.listdir(DIR):
     nf = nf.replace("part", "").replace("_en_to_kha.txt", "")
     nf = int(nf)
     with open(f"{DIR}/{file}", "r", encoding="utf-8") as fp:
-        LC += len(fp.readlines())
+        clc = len(fp.readlines())
+        if clc > 1250:
+            print(file)
+        LC += clc
 
 print(f"Lines translated : {LC}/{5000000}")
